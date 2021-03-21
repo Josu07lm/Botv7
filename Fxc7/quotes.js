5c3095054e7a7da47e02b087029337ebec025ce6
alexmlvalmv <77933997+alexmlvalmv@users.noreply.github.com>
2021-03-21 16:32:38 -0700

    Add files via upload

---
+++ Fxc7/quotes.js
@@ -0,0 +1,128 @@
+[
+  {
+    "autor": "A. Francia",
+    "quotes": "Es mejor entender un poco que malinterpretar"
+  },
+  {
+    "autor": "Abraham Lincoln",
+    "quotes": "Casi todos los hombres son capaces de resistir la adversidad. Sin embargo, si quieres probar el verdadero carácter de un hombre, dale poder"
+  },
+  {
+    "autor": "Esquilo",
+    "quotes": "Si la determinación de una persona es fuerte y firme, Dios se unirá a sus esfuerzos"
+  },
+  {
+    "autor": "Esopo",
+    "quotes": "El sufrimiento es una lección"
+  },
+  {
+    "autor": "Albert Einstein",
+    "quotes": "La ciencia sin religión está mutilada"
+  },
+  {
+    "autor": "Albert Einstein",
+    "quotes": "La vida es como una bicicleta, para mantener el equilibrio tenemos que seguir moviéndonos"
+  },
+  {
+    "autor": "Albert Einstein",
+    "quotes": "Las diferencias pasadas, presentes y futuras no son más que obstinadas ilusiones"
+  },
+  {
+    "autor": "Albert Einstein",
+    "quotes": "Una mesa, una silla, un cuenco de frutas y un violín; ¿qué más se necesita para hacer que alguien se sienta feliz?"
+  },
+  {
+    "autor": "Albert Enstein",
+    "quotes": "Ten piedad de los demás, sé duro contigo mismo"
+  },
+  {
+    "autor": "Alex Osborn",
+    "quotes": "La mejor manera de moverse es asignándose tareas"
+  },
+  {
+    "autor": "Alexander A. Bogomoletz",
+    "quotes": "No debemos perder el entusiasmo. La pasión es el estimulante más fuerte para el amor, la creación y el deseo de vivir más tiempo"
+  },
+  {
+    "autor": "Alexander Solzhenitsyn",
+    "quotes": "El hombre será feliz siempre que elija ser feliz"
+  },
+  {
+    "author": "Ali Javan",
+    "quotes": "No espero ser todo para todos. Solo quiero ser algo para alguien"
+  },
+  {
+    "autor": "Ali ibn Abi Talib",
+    "quotes": "Si el intelecto de una persona es perfecto, entonces sus palabras son un poco"
+  },
+  {
+    "autor": "Ali ibn Abi Talib",
+    "quotes": "Un hombre feliz puede ser un maestro para sí mismo, un auriga de sus pasiones y un capitán para el arca de su vida"
+  },
+  {
+    "autor": "Ali ibn Abi Talib",
+    "quotes": "Un amigo honesto vale más que la propiedad heredada de los antepasados"
+  },
+  {
+  },
+  {
+    "autor": "Henry David Thoreau",
+    "quotes": "La bondad es la única inversión que no hará daño"
+  },
+  {
+    "autor": "Henry Ford",
+    "quotes": "Los idealistas son personas que ayudan a otros a prosperar"
+  },
+  {
+    "autor": "Henry Ford",
+    "quotes": "Pensar es un trabajo duro entre todo tipo de trabajos. Por eso a tan poca gente le gusta hacerlo"
+  },
+  {
+    "autor": "Henry Ford",
+    "quotes": "La competencia cuyo propósito es solo competir, vencer a otras personas, nunca trae muchos beneficios"
+  },
+  {
+    "autor": "Henry Longfellow",
+    "quotes": "La vida de grandes personas nos recuerda que podemos hacer que nuestras vidas sean sublimes"
+  },
+  {
+    "autor": "Henry Thoreau",
+    "quotes": "Mi vida se ha convertido en mi entretenimiento y nunca deja de dar sorpresas. Mi vida es como un drama con tantos actos sin escenas finales"
+  },
+  {
+    "autor": "Hubert Humprey",
+    "quotes": "Lo que ves es lo que logras"
+  },
+  {
+    "Autor": "Imam Al-Ghazali",
+    "quotes": "La felicidad radica en ganar la lucha contra la lujuria y reprimir el exceso de deseo"
+  },
+  {
+    "Autor": "Imam Ghazali",
+    "quotes": "Jurar ante un criminal es un honor para una persona honesta"
+  },
+  {
+    "autor": "J.C.F von Schiller",
+    "quotes": "Quien medita demasiado agarra poco"
+  },
+  {
+    "author": "Jack Hyles",
+    "quotes": "No utilice a las personas para crear grandes trabajos, utilice su trabajo para formar grandes personas"
+  },
+  {
+    "autor": "Jackson Brown",
+    "quotes": "El mayor error que puede cometer es creer que trabaja para otra persona"
+  },
+  {
+    "autor": "Jacques Audiberti",
+    "quotes": "La mayor cobardía es cuando demostramos nuestra fuerza a la debilidad de los demás"
+  },
+  {
+    "autor": "James Thurber",
+    "quotes": "No mires el pasado con pesar, no mires el futuro con miedo, mira a tu alrededor con plena conciencia"
+  },
+  {
+    "autor": "Janet Erskine",
+    "quotes": "No esperes la situación ideal. No esperes las mejores oportunidades. Ambas nunca llegarán"
+  },
+  ]
